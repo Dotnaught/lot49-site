c9a28af19d86345147e1e7cc7db535883d46e6a2
Thomas Claburn <tc@lot49.com>
2023-04-08 15:39:33 -0700

    Revision

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,20 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
 declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface Locals {}
+
+   interface Platform {
+       env: {
+           COUNTER: DurableObjectNamespace;
+       };
+       context: {
+           waitUntil(promise: Promise<any>): void;
+       };
+       caches: CacheStorage & { default: Cache }
+   }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface Session {}
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface Stuff {}
 }
