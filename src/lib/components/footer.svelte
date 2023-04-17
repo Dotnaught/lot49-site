0be28b9a3f2887f1f3d4c77d0a2d1c127e75643e
Thomas Claburn <tc@lot49.com>
2023-04-16 20:08:06 -0700

    Centered footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,5 @@
 <footer class="rounded-lg shadow m-4 dark:bg-gray-900">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+    <div class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
             <a href="/about" class="mr-4 hover:underline md:mr-6">About</a>
